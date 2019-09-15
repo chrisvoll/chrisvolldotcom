3c7f5c660f7a4d1035c18d96d69f8f11e6f7b9ac
Chris Voll <chris@chrisvoll.com>
2019-09-15 13:42:29 -0400

    hooked on hooks

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,40 +2,35 @@ import React from 'react';
 import PostList from '../components/PostList';
 import { HeaderGroup, Headline, Date } from '../util/style';
 import Layout from '../layouts/layout';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 export default function IndexPage() {
-  return (
-    <StaticQuery
-      query={graphql`
-        query IndexQuery {
-          allMarkdownRemark(
-            sort: { order: DESC, fields: [frontmatter___date] }
-          ) {
-            edges {
-              node {
-                excerpt(pruneLength: 250)
-                id
-                frontmatter {
-                  title
-                  date(formatString: "MMMM DD, YYYY")
-                  path
-                  tags
-                }
-              }
+  const data = useStaticQuery(graphql`
+    query IndexQuery {
+      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+        edges {
+          node {
+            excerpt(pruneLength: 250)
+            id
+            frontmatter {
+              title
+              date(formatString: "MMMM DD, YYYY")
+              path
+              tags
             }
           }
         }
-      `}
-      render={data => (
-        <Layout>
-          <HeaderGroup>
-            <Headline>chrisvoll</Headline>
-            <Date>web developer @ seatgeek</Date>
-          </HeaderGroup>
-          <PostList posts={data.allMarkdownRemark.edges} />
-        </Layout>
-      )}
-    />
+      }
+    }
+  `);
+
+  return (
+    <Layout>
+      <HeaderGroup>
+        <Headline>chrisvoll</Headline>
+        <Date>web developer @ seatgeek</Date>
+      </HeaderGroup>
+      <PostList posts={data.allMarkdownRemark.edges} />
+    </Layout>
   );
 }
--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -1,57 +1,52 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import WithFadeTransition from '../components/WithFadeTransition';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import { HeaderGroup, Tag, Headline, Date } from '../util/style';
 
 import 'prismjs/themes/prism-solarizedlight.css';
 
 export default function BlogPost() {
-  return (
-    <StaticQuery
-      query={graphql`
-        query BlogPostByPath($path: String!) {
-          markdownRemark(frontmatter: { path: { eq: $path } }) {
-            html
-            excerpt(pruneLength: 160)
-            frontmatter {
-              date(formatString: "MMMM DD, YYYY")
-              path
-              title
-              tags
-            }
-          }
+  const data = useStaticQuery(graphql`
+    query BlogPostByPath($path: String!) {
+      markdownRemark(frontmatter: { path: { eq: $path } }) {
+        html
+        excerpt(pruneLength: 160)
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          path
+          title
+          tags
         }
-      `}
-      render={data => {
-        const post = data && data.markdownRemark;
-        const category = post.frontmatter.tags[0];
-        const meta = [
-          {
-            name: 'description',
-            content: post.excerpt
-          },
-          {
-            name: 'keywords',
-            content: post.frontmatter.tags.join(', ')
-          }
-        ];
-        return (
-          <WithFadeTransition>
-            <Helmet title={post.frontmatter.title} meta={meta} />
-            <HeaderGroup>
-              <Tag to={`/tags/${category}`}>{category}</Tag>
-              <Headline>{post.frontmatter.title}</Headline>
-              <Date>{post.frontmatter.date}</Date>
-            </HeaderGroup>
-            <div
-              className="post-content"
-              dangerouslySetInnerHTML={{ __html: post.html }}
-            />
-          </WithFadeTransition>
-        );
-      }}
-    />
+      }
+    }
+  `);
+
+  const post = data && data.markdownRemark;
+  const category = post.frontmatter.tags[0];
+  const meta = [
+    {
+      name: 'description',
+      content: post.excerpt
+    },
+    {
+      name: 'keywords',
+      content: post.frontmatter.tags.join(', ')
+    }
+  ];
+  return (
+    <WithFadeTransition>
+      <Helmet title={post.frontmatter.title} meta={meta} />
+      <HeaderGroup>
+        <Tag to={`/tags/${category}`}>{category}</Tag>
+        <Headline>{post.frontmatter.title}</Headline>
+        <Date>{post.frontmatter.date}</Date>
+      </HeaderGroup>
+      <div
+        className="post-content"
+        dangerouslySetInnerHTML={{ __html: post.html }}
+      />
+    </WithFadeTransition>
   );
 }
